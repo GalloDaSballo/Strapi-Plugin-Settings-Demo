ad04fe5b545a42f00d96831fcd7c3f75b341e2dc
Entreprenerd <alex@entreprenerd.xyz>
2020-08-04 14:06:28 +0200

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3d37f62c95147740967910979c43d5f5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8ff3d25e-e4cb-44cf-b310-f49b8b3e55d9'
+};
